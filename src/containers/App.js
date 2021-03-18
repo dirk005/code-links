75b7ed99e0a26908e4b6d96ab726e1300bc1b7f9
Dirk Jansen van Rensburg <dirk.jvrensburg@arwyp.com>
2021-03-18 15:15:23 +0200

    Change app

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Usefull Code links</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
