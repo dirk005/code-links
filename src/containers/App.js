483573c3a176a9028775c40e2834e1833516d9a1
Dirk Jansen van Rensburg <dirk.jvrensburg@arwyp.com>
2021-03-18 16:00:00 +0200

    add links

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,43 @@
+import React, { Component } from "react";
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      links: [],
+    };
+  }
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Usefull Code links</h1>
-      </header>
-    </div>
-  );
-};
+  componentDidMount() {
+    this.getLinks();
+  }
+
+  getLinks = () => {
+    fetch("https://dirk005.github.io/Projects-json/json/links.json")
+      .then((resp) => resp.json())
+      .then((links) => this.setState({ links: links }))
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Usefull Code links</h1>
+          {this.state.links.map((link) => (
+            <div>
+              <img alt={link.imageLink} src={link.imageLink} />
+              <h2>{link.name}</h2>
+              <p>{link.description}</p>
+              <a href={link.link} target="_blank" rel="noreferrer">
+                {link.link}
+              </a>
+            </div>
+          ))}
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
